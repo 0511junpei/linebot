836ce9390162d83d533616988c2124a6cebed3ae
0511junpei <118587464+0511junpei@users.noreply.github.com>
2024-11-13 22:29:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # 参考：https://qiita.com/nozomiyamada/items/87389aacb151b8418501
 from flask import Flask, request, abort
-from linebot.v3 import WebhookHandler
+from linebot import  LineBotApi
+from linebot.v3 import  WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.messaging import (
 	ApiClient, Configuration, MessagingApi,
@@ -8,7 +9,7 @@ from linebot.v3.messaging import (
 	TextMessage, PostbackAction
 )
 from linebot.v3.webhooks import (
-	FollowEvent, MessageEvent, PostbackEvent, TextMessageContent
+	FollowEvent, MessageEvent, PostbackEvent, TextMessageContent, ImageMessageContent
 )
 from dotenv import load_dotenv
 import os
@@ -53,21 +54,29 @@ def handle_message(event):
         line_bot_api = MessagingApi(api_client)
     
     received_message = event.message.text
-    received_image_id = event.message.id
 
 	## APIを呼んで送信者のプロフィール取得
-    # profile = line_bot_api.get_profile(event.source.user_id)
-    # display_name = profile.display_name
+    profile = line_bot_api.get_profile(event.source.user_id)
+    display_name = profile.display_name
 
 	## 返信メッセージ編集
-    # reply = f'{display_name}さんのメッセージ\n{received_message}'
-    reply = received_image_id
+    reply = f'{display_name}さんのメッセージ\n{received_message}'
 
 	## オウム返し
     line_bot_api.reply_message(ReplyMessageRequest(
 		replyToken=event.reply_token,
 		messages=[TextMessage(text=reply)]
 	))
+
+# 参考：https://qiita.com/tamago324/items/4df361fd6ac5b51a8a07
+@handler.add(MessageEvent, message=ImageMessageContent)
+def handle_image(event):
+    line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+    message_id = event.message.id
+    message_content = line_bot_api.get_message_content(message_id)
+    with open('file', 'w') as f:
+            for c in message_content.iter_content():
+                f.write(c)
 
 @app.route('/', methods=['GET'])
 def toppage():
