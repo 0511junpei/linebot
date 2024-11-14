0c3fbbb9987b256d299fc188361cfa79d625cf16
0511junpei <118587464+0511junpei@users.noreply.github.com>
2024-11-15 00:22:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # 参考：https://qiita.com/nozomiyamada/items/87389aacb151b8418501
+from pathlib import Path
 from flask import Flask, request, abort
 from linebot import  LineBotApi
 from linebot.v3 import  WebhookHandler
@@ -68,21 +69,49 @@ def handle_message(event):
 		messages=[TextMessage(text=reply)]
 	))
 
+SRC_IMAGE_PATH = "static/images/{}.jpg"
+MAIN_IMAGE_PATH = "static/images/{}_main.jpg"
+PREVIEW_IMAGE_PATH = "static/images/{}_preview.jpg"
+
 # 参考：https://qiita.com/tamago324/items/4df361fd6ac5b51a8a07
 @handler.add(MessageEvent, message=ImageMessageContent)
 def handle_image(event):
     with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApiBlob(api_client)
-        messaging_api = MessagingApi(api_client)
+        line_bot_api = LineBotApi(api_client)
     message_id = event.message.id
-    test = line_bot_api.get_message_content_with_http_info(message_id)
-    line_bot_api.get_message_content_preview(message_id)
 
-    messaging_api.reply_message(ReplyMessageRequest(
-		replyToken=event.reply_token,
-		messages=[TextMessage(text=test)]
-	))
-	
+    src_image_path = Path(SRC_IMAGE_PATH.format(message_id)).absolute()
+    main_image_path = MAIN_IMAGE_PATH.format(message_id)
+    preview_image_path = PREVIEW_IMAGE_PATH.format(message_id)
+
+    # 画像を保存
+    save_image(message_id, src_image_path)
+
+    # 画像の加工、保存
+    date_the_image(src=src_image_path, desc=Path(main_image_path).absolute())
+    date_the_image(src=src_image_path, desc=Path(preview_image_path).absolute())
+
+    # 画像の送信
+    image_message = ImageSendMessage(
+        original_content_url=f"https://date-the-image.herokuapp.com/{main_image_path}",
+        preview_image_url=f"https://date-the-image.herokuapp.com/{preview_image_path}",
+    )
+
+    app.logger.info(f"https://date-the-image.herokuapp.com/{main_image_path}")
+    line_bot_api.reply_message(event.reply_token, image_message)
+
+    # 画像を削除する
+    src_image_path.unlink()
+
+def save_image(message_id: str, save_path: str) -> None:
+    with ApiClient(configuration) as api_client:
+        line_bot_api = LineBotApi(api_client)
+    """保存"""
+    message_content = line_bot_api.get_message_content(message_id)
+    with open(save_path, "wb") as f:
+        for chunk in message_content.iter_content():
+            f.write(chunk)
+
 @app.route('/', methods=['GET'])
 def toppage():
     return 'Hello World'
