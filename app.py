69c2a8802b3154d960a4ead1d9e0b72d2d1a1d29
0511junpei <118587464+0511junpei@users.noreply.github.com>
2025-01-18 22:09:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -90,6 +90,8 @@ def handle_image(event):
     message_id = event.message.id
     SRC_IMAGE_PATH = "static/images/{}.jpg"
     src_image_path = Path(SRC_IMAGE_PATH.format(message_id)).absolute()
+    # 画像を保存
+    save_image(message_id, src_image_path)
 	
     genai.configure(api_key=GEMINI_API_KEY)
     model = genai.GenerativeModel("gemini-1.5-flash")
@@ -105,6 +107,13 @@ def handle_image(event):
 		replyToken=event.reply_token,
 		messages=[TextMessage(text=response.text)]
     ))
+
+def save_image(message_id: str, save_path: str) -> None:
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApiBlob(api_client)
+    message_content = line_bot_api.get_message_content(message_id)
+    with open(save_path, "wb") as f:
+        f.write(message_content)
 
 @app.route('/', methods=['GET'])
 def toppage():
